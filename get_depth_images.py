3d699b5b79734a30bef46d1295cd1cc6d1150f08
Junwei Liang <junweiliang1114@gmail.com>
2024-08-02 09:52:56 +0800

    updated

--- get_depth_images.py
+++ get_depth_images.py
@@ -18,6 +18,18 @@ parser.add_argument("--camera_type", default="realsense")
 #   [ WARN:0@0.012] global cap_v4l.cpp:999 open VIDEOIO(V4L2:/dev/video0): can't open camera by index
 # 可能需要在笔记本电脑上登录一下你的账号，唤醒一下摄像头
 
+
+def show_point_depth(point, depth_image, color_image):
+    depth = depth_image[point[0], point[1]]  # in milimeters
+    color_image = cv2.circle(
+        color_image, point, radius=2, color=(0, 255, 0), thickness=2)
+    color_image = cv2.putText(
+        color_image, "d: %smm" % depth,
+        (point[0], point[1] - 10), cv2.FONT_HERSHEY_SIMPLEX
+        fontScale=1, color=(0, 255, 0), thickness=2)
+    return color_image
+
+
 if __name__ == "__main__":
     args = parser.parse_args()
 
@@ -26,7 +38,7 @@ if __name__ == "__main__":
     if args.camera_type == "realsense":
 
         # 1. install realsense-viewer through here:
-        # https://www.intelrealsense.com/get-started-depth-camera/
+        # https://github.com/IntelRealSense/librealsense/blob/development/doc/installation.md
         # 2. pip install pyrealsense2
         import pyrealsense2 as rs
 
@@ -72,7 +84,7 @@ if __name__ == "__main__":
             depth_intrin = aligned_depth_frame.profile.as_video_stream_profile().intrinsics  # 获取深度参数（像素坐标系转相机坐标系会用到）
             color_intrin = aligned_color_frame.profile.as_video_stream_profile().intrinsics  # 获取相机内参
 
-            print(depth_intrin, color_intrin)
+            #print(depth_intrin, color_intrin)
 
             # Convert images to numpy arrays
             depth_image = np.asanyarray(depth_frame.get_data())
@@ -80,7 +92,14 @@ if __name__ == "__main__":
             # (480, 640, 3), (480, 640)
             # depth_image are in meters
             #print(color_image.shape, depth_image.shape)
-            print(depth_image[240, 320])
+            #print(depth_image[240, 320]) # 单位：毫米
+
+            # showing two points' depth
+            point1 = (200, 300)
+            point2 = (240, 320)
+
+            color_image = show_point_depth(point1, depth_image, color_image)
+            color_image = show_point_depth(point2, depth_image, color_image)
 
             # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
             depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
