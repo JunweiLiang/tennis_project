6aba655a2e35e15e204b3423c5681052ea5748cf
Junwei Liang <junweiliang1114@gmail.com>
2024-08-22 14:22:58 +0800

    updated

--- get_depth_images_orbbec.py
+++ get_depth_images_orbbec.py
@@ -42,6 +42,23 @@ def get_orbbec_depth_data(orbbec_depth_frame):
 
     return depth_data
 
+
+def get_orbbec_color_data(orbbec_color_frame):
+    # in BGR order
+    width = orbbec_color_frame.get_width()
+    height = orbbec_color_frame.get_height()
+    color_format = orbbec_color_frame.get_format()
+
+    assert color_format in [OBFormat.RGB]
+
+    if color_format == OBFormat.RGB:
+        data = np.asanyarray(orbbec_color_frame.get_data())
+        image = np.resize(data, (height, width, 3))
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    return image
+
+
 def get_point_position(data_dir, xy):
     colors = np.array(Image.open(os.path.join(data_dir, 'color.png')), dtype=np.float32) / 255.0
     depths = np.array(Image.open(os.path.join(data_dir, 'depth.png')))
@@ -79,22 +96,6 @@ def get_point_position(data_dir, xy):
     print('point:', center)
 
     return center
-
-def get_orbbec_color_data(orbbec_color_frame):
-    # in BGR order
-    width = orbbec_color_frame.get_width()
-    height = orbbec_color_frame.get_height()
-    color_format = orbbec_color_frame.get_format()
-
-    assert color_format in [OBFormat.RGB]
-
-    if color_format == OBFormat.RGB:
-        data = np.asanyarray(orbbec_color_frame.get_data())
-        image = np.resize(data, (height, width, 3))
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-    return image
-
 
 
 if __name__ == "__main__":
@@ -175,58 +176,16 @@ if __name__ == "__main__":
             depth_data = get_orbbec_depth_data(depth_frame)
             color_data = get_orbbec_color_data(color_frame)
             #print(depth_data[depth_data != 0]) # in 毫米
-            print(depth_data.shape) # (576, 640)
-            print(color_data.shape) # (1080, 1920, 3)
+            #print(depth_data.shape) # (960, 1280)
+            #print(color_data.shape) # (960, 1280, 3)
 
-            break
-            # junwei: the color_intrin and depth_intrin are the same as they are aligned.
-            #### 获取相机参数 ####
-            aligned_depth_frame = aligned_frames.get_depth_frame()  # 获取对齐帧中的的depth帧
-            aligned_color_frame = aligned_frames.get_color_frame()  # 获取对齐帧中的的color帧
-            depth_intrin = aligned_depth_frame.profile.as_video_stream_profile().intrinsics  # 获取深度参数（像素坐标系转相机坐标系会用到）
-            # [ 640x480  p[325.217 238.38]  f[385.38 384.848]  Inverse Brown Conrady [-0.0565123 0.067672 0.000208852 0.000719325 -0.0218305] ]
-            color_intrin = aligned_color_frame.profile.as_video_stream_profile().intrinsics  # 获取相机内参
+            # 1. print out the depth at the center
+            point1 = (480, 640)  # (y, x)
 
-            #print(depth_intrin, color_intrin)
-
-
-            # (480, 640, 3), (480, 640)
-            # depth_image are in meters
-            #print(color_image.shape, depth_image.shape)
-            #print(depth_image[240, 320]) # 单位：毫米
-
-            # showing two points' depth
-            point1 = (200, 200)  # (y, x)
-            point2 = (240, 320)
-
-            color_image, depth1 = show_point_depth(point1, depth_image, color_image)
-            color_image, depth2 = show_point_depth(point2, depth_image, color_image)
-            # mm to meters
-            depth1 = depth1 * depth_scale
-            depth2 = depth2 * depth_scale
-
-            # rs2_deproject_pixel_to_point takes pixel (x, y)
-            # outputs (x, y, z), the coordinates are in meters
-            #   [0,0,0] is the center of the camera, 相机朝向的右边是正x，下边为正y, 朝向是正z
-            #   See this doc for coordinate system
-            #   https://github.com/IntelRealSense/librealsense/wiki/Projection-in-RealSense-SDK-2.0?fbclid=IwAR3gogVZe824YUps88Dzp02AN_XzEm1BDb0UbmzfoYvn1qDFb7KzbIz9twU#point-coordinates
-            # 理解此函数，需要知道camera model，perspective projection, geometric computer vision
-            # 也就是说3D世界的坐标如何与相机上的像素坐标互相转换的
-            point1_3d = rs.rs2_deproject_pixel_to_point(depth_intrin, (point1[1], point1[0]), depth1)
-            point2_3d = rs.rs2_deproject_pixel_to_point(depth_intrin, (point2[1], point2[0]), depth2)
-
-            # 计算这两点的实际距离
-            #print(point1_3d, point2_3d)
-            dist_between_point1_point2 = np.linalg.norm(np.array(point1_3d) - np.array(point2_3d))
-
-            mid_point_xy = ( int((point2[1] + point1[1])/2.), int((point2[0] + point1[0])/2.) + 50)
-            color_image = cv2.putText(
-                color_image, "dist 1to2: %.2f meters" % dist_between_point1_point2,
-                mid_point_xy, cv2.FONT_HERSHEY_SIMPLEX,
-                fontScale=1, color=(0, 0, 255), thickness=2)
+            color_image, depth1 = show_point_depth(point1, depth_data, color_data)
 
             # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_data, alpha=0.03), cv2.COLORMAP_JET)
 
             # Stack both images horizontally
             image = np.hstack((color_image, depth_colormap))
